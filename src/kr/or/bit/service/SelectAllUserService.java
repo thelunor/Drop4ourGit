92fe4c336e49b5637c5790c1f22d1fb2ae2265d7
hyungnam <zzzsh789@naver.com>
2019-11-20 18:22:07 +0900

    SelectAllUserOkService 추가 컨트롤러 SelectAllUserService, SelectAllUserOkService 수정

--- src/kr/or/bit/service/SelectAllUserService.java
+++ src/kr/or/bit/service/SelectAllUserService.java
@@ -1,17 +1,34 @@
 package kr.or.bit.service;
 
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+
 import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
+import kr.or.bit.dao.UserDao;
+import kr.or.bit.dto.Users;
 
 public class SelectAllUserService implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		ActionForward forward = null;
+		List<Users> userList = null;
+		try {
+			UserDao dao = new UserDao();
+			userList = dao.selectAllUser();
+			request.setAttribute("userList", userList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		forward = new ActionForward();
+		forward.setPath("/SelectAllUserOkService.d4b");
+		return forward;
 	}
 
 }
