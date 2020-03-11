efb7cb2b9e98ee28134c2aa38e943e2c9b6a5374
hyeleelee <hgoguma124@gmail.com>
2020-03-11 14:54:53 +0900

    [#2] Gradle 수정 & template 파일 수정 & application.properties -> application.yml 대체

--- CustomizedHouse/src/main/java/com/scoder/hs/controller/MainController.java
+++ CustomizedHouse/src/main/java/com/scoder/hs/controller/MainController.java
@@ -13,44 +13,21 @@ import com.scoder.hs.service.UserServiceImpl;
 @Controller
 public class MainController {
 	
-	@Autowired
-	private UserServiceImpl userService;
-	
 	@GetMapping("/")
-    public String index() {
-        return "/index";
-    }
-		
+	public String index() {
+		return "index";
+	}
 	
-	@GetMapping("/user/login")
+	@GetMapping("/loginPage")
 	public String login() {
-		System.out.println("로그인??");
-		return "login";
+		return "loginPage";
 	}
 	
-	@GetMapping("/user/login/result")
-	public String loginResult() {
-		System.out.println("login �Ϸ� ������!");
-		return "loginSuccess";
-	}
-	
-	
-	
-	@RequestMapping(value = "/user/greeting", method = RequestMethod.GET)
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		System.out.println("��Ʈ�ѷ� Ÿ��?"+name.toString());
-        model.addAttribute("name", name);		
-		return "greeting";   
-	}
-	
-	@RequestMapping(value = "/user/object", method = RequestMethod.GET)
-	public String greeting(Model model) {
-		return "object";   
-	}
-	
-	@RequestMapping(value = "/user/save", method = RequestMethod.GET)
-	public String save(Model model) {
-		return "save";  
+
+
+	@GetMapping("admin/adminPage")
+	public String adminPage() {
+		return "adminPage";
 	}
 	
 	
