1515e68abfe1fc43bb4b4e52eaa77327109aa8a5
Lee JeongEun <jelee1223@naver.com>
2020-03-11 15:04:28 +0900

    Sale Domain 설정

--- CustomizedHouse/src/main/java/com/scoder/hs/domain/Sale.java
+++ CustomizedHouse/src/main/java/com/scoder/hs/domain/Sale.java
@@ -21,6 +21,38 @@ public class Sale {
 	@Column(name="userid", updatable=false, nullable=false)
 	private String userId; //공인중개사 아이디
 	
-
+	@Column(updatable=true, nullable = false)
+	private int typeNum; //거래유형번호
+	
+	@Column(length = 100, nullable = false)
+	private String aptSize; //면적
+	
+	@Column(columnDefinition = "SALEADDRESS", nullable = false)
+	private String address; //지번주소
+	
+	@Column(columnDefinition = "SALEROADADDRESS", nullable = false)
+	private String roadAddress; //도로명주소
+	
+	@Column(length = 500, nullable = false)
+	private String aptName; //아파트 이름
+	
+	@Column(length = 500, nullable = false)
+	private String aptDong; //동
+	
+	@Column(length = 500, nullable = false)
+	private String aptHo; //호	
+	
+	@Column(columnDefinition = "SALEPRICE", nullable = false)
+	private String price; //거래금액
+	
+	@Column(columnDefinition = "SALEDIRECTION", nullable = false)
+	private String direction; //향
+	
+	@Column(columnDefinition = "SALEETC", nullable = false)
+	private String etc; //매물 특징
+	
+	@Column(updatable=true, nullable = false)
+	private String isContract; //계약 여부
+	
 
 }
